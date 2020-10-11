7387aa06b10e38319d54ba601f36ba7636489c95
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-10-11 00:28:42 -0600

    2.8-9 Loading Window file and adding icon, and absolute paths

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,11 +1,21 @@
 const { app, BrowserWindow } = require('electron');
 
+let mainWindow;
+
 function createMainWindow() {
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     title: 'ImageShrink',
     width: 500,
     height: 600,
+    icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    webPreferences: {
+      worldSafeExecuteJavaScript: true,
+      contextIsolation: true,
+    },
   });
+
+  // mainWindow.loadURL(`file://${__dirname}/app/index.html`);
+  mainWindow.loadFile(`${__dirname}/app/index.html`);
 }
 
 app.on('ready', createMainWindow);
