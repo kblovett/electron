0e221ea6905e8a2746873c747c1565414620c1c2
Kolin Lovett <47673147+kblovett@users.noreply.github.com>
2020-10-11 12:22:24 -0600

    2.10-11 Env and nodemon

--- image-shrink/main.js
+++ image-shrink/main.js
@@ -1,4 +1,13 @@
 const { app, BrowserWindow } = require('electron');
+const os = require('os');
+
+// set env
+process.env.NODE_ENV = 'production';
+const isDev = process.env.NODE_ENV !== 'production' ? true : false;
+const isMac = os.platform() === 'darwin' ? true : false;
+const isTux = os.platform() === 'linux' ? true : false;
+const isWin = os.platform() === 'win32' ? true : false;
+console.log(isDev, isMac, isTux, isWin);
 
 let mainWindow;
 
@@ -8,6 +17,7 @@ function createMainWindow() {
     width: 500,
     height: 600,
     icon: `${__dirname}/assets/icons/Icon_256x256.png`,
+    resizable: isDev,
     webPreferences: {
       worldSafeExecuteJavaScript: true,
       contextIsolation: true,
@@ -19,3 +29,15 @@ function createMainWindow() {
 }
 
 app.on('ready', createMainWindow);
+app.on('window-all-closed', () => {
+  if (!isMac) {
+    app.quit();
+  }
+});
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createMainWindow();
+  }
+});
+
+app.allowRendererProcessReuse = true;
